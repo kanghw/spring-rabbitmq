9bc37bead47b0ff9040be85b379ef60f755b2127
강현욱 <kangnara@joonggonara.co.kr>
2021-04-06 23:29:06 +0900

    MessageListener Bean 등록하여 사용하는 방식

--- src/main/java/com/example/springrabbitmq/config/RabbitmqConfig.java
+++ src/main/java/com/example/springrabbitmq/config/RabbitmqConfig.java
@@ -1,15 +1,25 @@
 package com.example.springrabbitmq.config;
 
-import org.springframework.amqp.core.AcknowledgeMode;
+import java.nio.charset.StandardCharsets;
+import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
 public class RabbitmqConfig {
+
+    @Bean
+    public SimpleMessageListenerContainer messageListenerContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(rabbitConnectionFactory());
+        container.setQueueNames("testQueue");
+        container.setMessageListener(exampleListener());
+        return container;
+    }
 
     @Bean
     public CachingConnectionFactory rabbitConnectionFactory() {
@@ -21,14 +31,10 @@ public class RabbitmqConfig {
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(rabbitConnectionFactory());
-        factory.setConcurrentConsumers(3);
-        factory.setMaxConcurrentConsumers(10);
-        factory.setContainerCustomizer(container -> {
-            container.setAcknowledgeMode(AcknowledgeMode.AUTO);
-        });
-        return factory;
+    public MessageListener exampleListener() {
+        return message -> {
+            System.out.println("received: " + message);
+            System.out.println(new String(message.getBody(), StandardCharsets.UTF_8));
+        };
     }
 }
